003ee70e9d4ed87ed694ab16df013170ce354785
vatokaramyan-step <vato.karamyan@gmail.com>
2026-03-11 08:42:02 +0000

    b

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,11 @@
 
-
-from django.contrib import admin
+from homepage.views import index
+# from django.contrib import admin
 from django.urls import path , include
 
 
 urlpatterns = [
-    path('', include('homepage.urls')),
+    path('', index, name='index'),
+    # path('', include('homepage.urls')),
 #     path("admin/", admin.site.urls),  
 ]
