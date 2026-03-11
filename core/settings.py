2394f6be221ee685d3dcf5c3139363f658f73f8b
vatokaramyan-step <vato.karamyan@gmail.com>
2026-03-11 10:01:58 +0000

    g

--- core/settings.py
+++ core/settings.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "homepage.apps.HomepageConfig",
+    "snippets",
     "rest_framework",
     "todos",
     "apis",
